798f84d31d578699ee5dc17aa3c2b8beae8e8c94
Gabe G <gabesellsnj@gmail.com>
2023-01-23 18:53:41 -0500

    Code complete

--- app.py
+++ app.py
@@ -1,3 +1,57 @@
 import pandas as pd
-import streamlit as sl
+import streamlit as st
 import plotly.express as px
+
+df = pd.read_csv('vehicles_us.csv')
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+
+st.header('Data viewer')
+st.dataframe(df)
+
+#distribution of vehicle type by the manufacturer
+st.header('Vehicle types by manfacturer')
+#create a plotly histogram figure
+fig = px.histogram(df, x ='manufacturer', color = 'type')
+#display the figure with streamlit
+st.write(fig)
+
+#histogram of condition vs model year
+st.header("Histogram of `condition` vs `model_year`")
+fig2 = px.histogram(df, x = 'model_year', color = 'condition')
+st.write(fig2)
+
+st.header('Compare price distribution between manufacturers')
+# get a list of car manufacturers
+manufac_list = sorted(df['manufacturer'].unique())
+# get user's inputs from a dropdown menu
+manufacturer_1 = st.selectbox(
+                              label='Select manufacturer 1', # title of the select box
+                              options=manufac_list, # options listed in the select box
+                              index=manufac_list.index('chevrolet') # default pre-selected option
+                              )
+# repeat for the second dropdown menu
+manufacturer_2 = st.selectbox(
+                              label='Select manufacturer 2',
+                              options=manufac_list, 
+                              index=manufac_list.index('hyundai')
+                              )
+# filter the dataframe 
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+
+# add a checkbox if a user wants to normalize the histogram
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+
+# create a plotly histogram figure
+fig3 = px.histogram(df_filtered,
+                      x='price',
+                      nbins=30,
+                      color='manufacturer',
+                      histnorm=histnorm,
+                      barmode='overlay')
+# display the figure with streamlit
+st.write(fig3)
